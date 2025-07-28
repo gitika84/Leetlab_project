40b9ae60fabeb7a6caefd4b8c8c693cd72be9ece
gitika84 <gitikasharma8j@gmai.com>
2025-07-28 16:09:08 +0530

    add custom ApiError class for standardized error handling

---
+++ backend/src/utils/api-errors.js
@@ -0,0 +1,14 @@
+class ApiError extends Erorr{
+    constructor(statusCode , message , errors = [] , stack = " "){
+        super(message)
+        this.statusCode = statusCode
+        this.message = message
+        this.errors = errors
+
+        if(stack){
+            this.stack = stack
+        }else{
+            Error.CaptureStackTrace(this , this.constructor)
+        }
+    }
+}

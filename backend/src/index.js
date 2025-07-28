7d9784ec1138f356765bd8787d1b38faaed3d120
gitika84 <gitikasharma8j@gmai.com>
2025-07-28 16:47:50 +0530

    configure global middlewares and add healthcheck route in main file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,27 @@
 import express from "express"
 import dotenv from "dotenv"
-
+import cors from "cors"
+import healthcheckRoutes from "./routes/healthcheck.routes.js"
 dotenv.config()
 
 const app = express()
 
 const port = process.env.PORT
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
+
+app.use("/api/v1/health", healthcheckRoutes)
+
 app.listen(port , () => {
     console.log(`Server is running on PORT ${port}`)
-}) 
+})
 
 
 

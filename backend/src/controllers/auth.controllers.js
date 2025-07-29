9755e3ed7d1e8984644dc98bc3efa094d17ff655
gitika84 <gitikasharma8j@gmai.com>
2025-07-29 19:09:46 +0530

    add login/logout controllers and middleware to protect routes

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -65,15 +65,66 @@ const registerUser = asyncHandler(async (req, res) => {
 })
 
 const loginUser = asyncHandler(async (req, res) => {
+    const { email , password } = req.body
+
+    const user = await db.user.findUnique({
+        where : { email }
+    })
+
+    if(!user){
+        return res.status(400).json(
+            new ApiError(400, "User does not exist")
+        )
+    }
+
+    const isMatched = await bcrypt.compare(password , user.password)
+
+    if (!isMatched) {
+        return res.status(400).json(
+            new ApiError(400, "Invalid credentials")
+    )
+    }
+
+    const token = jwt.sign({ id: user.id},
+        process.env.JWT_SECRET,
+        { expiresIn : "7d"}
+    )
+
+    res.cookie("jwt" , token , {
+        httpOnly : true,
+        sameSite : "strict",
+        secure : process.env.NODE_ENV !== "development",
+        maxAge : 1000 * 60 * 60 * 24 * 7 // 7 days
+    })
+
+    res.status(201).json(
+        new ApiResponse(201 , { 
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            image : user.image,
+        },"User login Successfully")
+    )
 
 })
 
 const logoutUser = asyncHandler(async (req, res) => {
+    res.clearCookie("jwt", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",     
+        sameSite: "strict"
+    })
 
+    res.status(200).json(
+        new ApiResponse(200 , {} , "User logout Successfully")
+    )
 })
 
 const check = asyncHandler(async (req, res) => {
-
+    res.status(200).json(
+        new ApiResponse(200 , {user : req.user} , "User Authenticated Successfully")
+    )
 })
 
 export { registerUser , loginUser , logoutUser , check }
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { registerUser, loginUser, logoutUser, check } from "../controllers/auth.controllers.js"
+import { authMiddleware } from "../middlewares/auth.middlewares.js"
 
 const router = express.Router()
 
 router.post("/register" , registerUser)
-router.get("/login" , loginUser)
-router.get("/logout" , logoutUser)
-router.get("/check" , check)
+router.post("/login", loginUser)
+router.get("/logout", authMiddleware, logoutUser)
+router.get("/check", authMiddleware , check)
 
 export default router
